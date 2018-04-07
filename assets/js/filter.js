44b3ea3de1a208c9c7fffc88451896b33c7ab422
Jon Bake <jonmbake@gmail.com>
2018-04-06 20:18:14 -0500

    Add Topic Filter

--- assets/js/filter.js
+++ assets/js/filter.js
@@ -4,33 +4,33 @@ $(document).ready(function() {
     {className: 'java', text: 'Java'},
     {className: 'javascript', text: 'JavaScript'},
     {className: 'medium', text: 'Medium'},
-    {className: 'personal_dev', text: 'Personal Development'},
+    {className: 'personal_dev', text: 'Personal Dev'},
     {className: 'security', text: 'Web Security'},
-    {className: 'software_dev', text: 'Software Development'},
+    {className: 'software_dev', text: 'Software Dev'},
     {className: 'sql', text: 'SQL'},
     {className: 'walkthroughs', text: 'Walk Throughs'},
   ];
 
   const POST_SELECTOR = '.post';
 
-  const filter = function () {
-    const selected = $('.topic-filter').select2("val");
-    if (selected.length === 0) {
-      $(POST_SELECTOR).show();
-    } else {
-      $(POST_SELECTOR).hide();
-      selected.forEach(s => $(POST_SELECTOR + '.' + s).show())
-    }
-  }
-
-  $('.topic-filter').select2({
-    width: '50%',
-    placeholder: 'Filter by Topic',
-    data: TOPICS.map((t, i) => {
-      return {
-        id: t.className,
-        text: t.text + ' (' + $(POST_SELECTOR + '.' + t.className).length + ')'
+  const buttons = TOPICS.map((t, i) => {
+    const selector = $(POST_SELECTOR + '.' + t.className);
+    const $b = $('<button class="filter-btn pure-button" data-classname=".post.' + t.className + '"><i class="filter-icon fa fa-circle-o"></i> ' + t.text + ' (' + selector.length + 'X)</button>');
+    $b.on('click', function() {
+      if ($(this).hasClass('filter-btn-active')) {
+        $(this).removeClass('filter-btn-active').find('.filter-icon').removeClass('fa-check-circle-o').addClass('fa-circle-o');
+      } else {
+        $(this).addClass('filter-btn-active').find('.filter-icon').removeClass('fa-circle-o').addClass('fa-check-circle-o');
+        selector.show();
       }
-    })
-  }).on('change', filter);
+      if ($('.filter-btn-active').length === 0) {
+        $(POST_SELECTOR).show();
+      } else {
+        $(POST_SELECTOR).hide();
+        $($('.filter-btn-active').map((i, a) => $(a).data('classname')).toArray().join(',')).show();
+      }
+    });
+    return $b;
+  })
+  $('.topic-filter').append(buttons);
 });
--- index.md
+++ index.md
@@ -3,13 +3,20 @@ layout: default
 ---
 <h1 class="posts-header">Posts</h1>
 
-<div class="posts">
-  {% for post in site.posts %}
-    <div class="post {% if post.layout == 'medium' %}medium{% endif %} {{ post.tags | join }}">
-        <h2><a href="{% if post.layout == 'medium' %}{{ post.medium_url }}{% else %}{{ site.baseurl }}{{ post.url }}{% endif %}">{{ post.title }}</a></h2>
-        <div>
-          <time>{{ post.date | date_to_string | upcase }} </time>{% include post-tags.html %}
+<div class="pure-g">
+  <div class="posts pure-u-4-5">
+    {% for post in site.posts %}
+      <div class="post {% if post.layout == 'medium' %}medium{% endif %} {{ post.tags | join }}">
+          <h2><a href="{% if post.layout == 'medium' %}{{ post.medium_url }}{% else %}{{ site.baseurl }}{{ post.url }}{% endif %}">{{ post.title }}</a></h2>
+          <div>
+            <time>{{ post.date | date_to_string | upcase }} </time>{% include post-tags.html %}
+          </div>
         </div>
-      </div>
-  {% endfor %}
+    {% endfor %}
+  </div>
+  <div class="posts pure-u-1-5">
+    <div class="filter-header">Filter by Topic</div>
+    <div class="topic-filter">
+    </div>
+  </div>
 </div>
