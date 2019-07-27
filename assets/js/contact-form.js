d4371160e56b429e977b76b87e3edeb27228fe68
Jon Bake <jonmbake@gmail.com>
2019-07-27 14:27:53 -0500

    Send feedback to Slack

--- assets/js/contact-form.js
+++ assets/js/contact-form.js
@@ -1,4 +1,5 @@
 (function () {
+    const loaded = new Date().getTime();
     var contactFormUtils = {
         isValidEmail: function (email) {
             var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
@@ -10,70 +11,60 @@
             $('#feedbackForm .form-group').removeClass('has-error');
         },
         clearForm: function () {
-            $('#feedbackForm .glyphicon').removeClass('glyphicon-check').addClass('glyphicon-unchecked').css({color: ''});
             $('#feedbackForm input,textarea').val("");
         },
         addError: function ($input) {
             var parentFormGroup = $input.parents('.form-group');
             parentFormGroup.children('.help-block').show();
             parentFormGroup.addClass('has-error');
-        },
-        addAjaxMessage: function(msg, isError) {
-            $("#feedbackSubmit").after('<div id="emailAlert" class="alert alert-' + (isError ? 'danger' : 'success') + '" style="margin-top: 5px;">' + $('<div/>').text(msg).html() + '</div>');
         }
     };
 
     $(document).ready(function() {
-        if ($("#phone").intlTelInput) {
-            $("#phone").intlTelInput({validationScript: "assets/vender/intl-tel-input/js/isValidNumber.js"});
-            $(".intl-tel-input.inside").css('width', '100%');
-        }
-
         $("#feedbackSubmit").click(function() {
             var $btn = $(this);
             $btn.prop("disabled",true);
             contactFormUtils.clearErrors();
 
-            //do a little client-side validation -- check that each field has a value and e-mail field is in proper format
-            //use bootstrap validator (https://github.com/1000hz/bootstrap-validator) if provided, otherwise a bit of custom validation
-            var $form = $("#feedbackForm"),
-                hasErrors = false;
-            if ($form.validator) {
-                hasErrors =  $form.validator('validate').hasErrors;
-            } else {
-                $('#feedbackForm input,#feedbackForm textarea').not('.optional').each(function() {
-                    var $this = $(this);
-                    if (($this.is(':checkbox') && !$this.is(':checked')) || !$this.val()) {
-                        hasErrors = true;
-                        contactFormUtils.addError($(this));
-                    }
-                });
-                var $email = $('#email');
-                if (!contactFormUtils.isValidEmail($email.val())) {
-                    hasErrors = true;
-                    contactFormUtils.addError($email);
-                }
-                var $phone = $('#phone');
-                if ($phone.val() && $phone.intlTelInput && !$phone.intlTelInput("isValidNumber")) {
-                    hasErrors = true;
-                    contactFormUtils.addError($phone.parent());
-                }
+            var $form = $("#feedbackForm"), hasErrors = false;
+            if (new Date().getTime() - loaded < 5000) {
+              hasErrors = true;
             }
-            //if there are any errors return without sending e-mail
+            $('#feedbackForm input,#feedbackForm textarea').not('.optional').each(function() {
+                var $this = $(this);
+                if (($this.is(':checkbox') && !$this.is(':checked')) || !$this.val()) {
+                    hasErrors = true;
+                    contactFormUtils.addError($(this));
+                }
+            });
+            var $email = $('#email');
+            if (!contactFormUtils.isValidEmail($email.val())) {
+                hasErrors = true;
+                contactFormUtils.addError($email);
+            }
             if (hasErrors) {
-                $btn.prop("disabled",false);
+                $btn.prop("disabled", false);
                 return false;
             }
-          $form.submit();
+
+            $.ajax({
+              url: 'https://godkqerucg.execute-api.us-east-1.amazonaws.com/Prod/feedback/',
+              dataType: 'json',
+              type: 'post',
+              contentType: 'application/json',
+              data: $form.serializeArray().reduce(function(p, c) {
+                p[c.name] = c.value;
+                return p;
+              }, {}),
+              success: function() {
+                $('#feedback-alert').removeClass('no-show alert-error').addClass('alert-success').text('Successfully submitted!');
+              },
+              error: function() {
+                $btn.prop("disabled", false);
+                $('#feedback-alert').removeClass('no-show alert-success').addClass('alert-error').text('Unexpected error while submitting feedback. Please try again later.');
+              }
+            });
             return false;
-        });
-        $('#feedbackForm input, #feedbackForm textarea').change(function () {
-            var checkBox = $(this).siblings('span.input-group-addon').children('.glyphicon');
-            if ($(this).val()) {
-                checkBox.removeClass('glyphicon-unchecked').addClass('glyphicon-check').css({color: 'green'});
-            } else {
-                checkBox.removeClass('glyphicon-check').addClass('glyphicon-unchecked').css({color: ''});
-            }
         });
     });
 })();
